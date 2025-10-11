f10cb08b6da14fbe74592116a4bebe660bf8c3f8
周睿 <zrufo747@outlook.com>
2025-10-11 16:27:48 +0800

    添加 Rockchip 电源管理模块，支持 rk3588 兼容性探测

--- src/rknpu/mod.rs
+++ src/rknpu/mod.rs
@@ -1,9 +1,8 @@
 use alloc::vec::Vec;
 
-use rdrive::{
-    DriverGeneric, PlatformDevice, module_driver, probe::OnProbeError, register::FdtInfo,
-};
+use rdrive::{PlatformDevice, module_driver, probe::OnProbeError, register::FdtInfo};
 use rknpu::{Rknpu, RknpuConfig, RknpuType};
+use rockchip_pm::{PD, RockchipPM};
 
 use crate::iomap;
 
@@ -47,8 +46,32 @@ fn probe(info: FdtInfo<'_>, plat_dev: PlatformDevice) -> Result<(), OnProbeError
         base_regs.push(unsafe { iomap(start as _, size)?.add(offset) });
     }
 
+    enable_pm();
+
+    info!("NPU power enabled");
+
     let npu = Rknpu::new(&base_regs, config);
     plat_dev.register(npu);
     info!("NPU registered successfully");
     Ok(())
+}
+
+fn enable_pm() {
+    // RK3588 NPU 相关电源域 ID
+
+    /// NPU 主电源域
+    pub const NPU: PD = PD(8);
+    /// NPU TOP 电源域  
+    pub const NPUTOP: PD = PD(9);
+    /// NPU1 电源域
+    pub const NPU1: PD = PD(10);
+    /// NPU2 电源域
+    pub const NPU2: PD = PD(11);
+
+    let mut pm = rdrive::get_one::<RockchipPM>().unwrap().lock().unwrap();
+
+    pm.power_domain_on(NPUTOP).unwrap();
+    pm.power_domain_on(NPU).unwrap();
+    pm.power_domain_on(NPU1).unwrap();
+    pm.power_domain_on(NPU2).unwrap();
 }
